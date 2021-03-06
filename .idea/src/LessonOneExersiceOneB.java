31df401b42b43c34ca78f35276457b7d787de710
Pingerock <65583521+Pingerock@users.noreply.github.com>
2021-03-06 16:25:50 +0300

    Lesson one Exersice One B

---
+++ .idea/src/LessonOneExersiceOneB.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LessonOneExersiceOneB {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите число: ");
+        int x = scanner.nextInt();
+
+        if (x >= 0) {
+            System.out.println("Положительное число: " + ++x);
+        } else {
+            System.out.println("Отрицательное число: " + x);
+        }
+        scanner.close();
+    }
+}
