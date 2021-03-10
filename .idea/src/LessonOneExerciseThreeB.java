3ce58e4fb18096740b487517dae11a32eb4bca36
Pingerock <65583521+Pingerock@users.noreply.github.com>
2021-03-10 19:12:57 +0300

    Lesson One Exercise Three B (reworked)

--- .idea/src/LessonOneExerciseThreeB.java
+++ .idea/src/LessonOneExerciseThreeB.java
@@ -4,23 +4,18 @@ public class LessonOneExerciseThreeB {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         float x, y;
-        boolean check = false;
+
         System.out.println("Введите переменную X: ");
         x = scanner.nextFloat();
         System.out.println("Введите переменную Y: ");
         y = scanner.nextFloat();
 
-        if (x >= -1) {
-            if (y <= 1) {
-                if ((y <= x) && (x <= y)) {
-                    System.out.println("Успех! Координаты попадают в область.");
-                    check = true;
-                }
-            }
-        }
-        if (!check) {
+        if ((x >= -1) && (y <= 1) && (y >= x)) {
+            System.out.println("Успех! Координаты попадают в область.");
+        } else {
             System.out.println("Провал. Координаты не попадают в область.");
         }
+
         scanner.close();
     }
 }
