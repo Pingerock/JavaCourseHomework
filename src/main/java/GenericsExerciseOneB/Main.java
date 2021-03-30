c399af487883d9d087b7a8d366aae5bf733d9507
Pingerock <65583521+Pingerock@users.noreply.github.com>
2021-03-30 20:54:34 +0300

    GenericsExerciseThreeB
    Исправленный вариант. Попытка №1.
    
    Список выводов:
    -упрощенн метод isEqualTo
    -узнали, что будет при использовании объекта со значением null

--- src/main/java/GenericsExerciseOneB/Main.java
+++ src/main/java/GenericsExerciseOneB/Main.java
@@ -7,20 +7,29 @@ public class Main {
         //создаем числа и объекты для испытания метода
         int x = 550;
         int x1 = 552;
-        Object z = 550;
+        Object z = null;
         Object z1 = 550;
+        Object z2 = 550;
         //применяем созданный универсальный метод isEqualTo
-        boolean test1, test2, test3, test4, test5;
-        test1 = task.isEqualTo(x, z);
+        boolean test1, test2, test3, test4, test5, test6, test7, test8, test9;
+        //test1 = task.isEqualTo(x, z);
         test2 = task.isEqualTo(x, x1);
-        test3 = task.isEqualTo(z, x);
-        test4 = task.isEqualTo(z, z1);
-        test5 = task.isEqualTo(z, x1);
+        //test3 = task.isEqualTo(z, x);
+        //test4 = task.isEqualTo(z, z1);
+        //test5 = task.isEqualTo(z, x1);
+        test6 = task.isEqualTo(x, z2);
+        test7 = task.isEqualTo(z2, x);
+        test8 = task.isEqualTo(z2, z1);
+        test9 = task.isEqualTo(z2, x1);
         //вывод
-        System.out.println(test1); //true
+        //System.out.println(test1); //NullPointerException
         System.out.println(test2); //false
-        System.out.println(test3); //true
-        System.out.println(test4); //true
-        System.out.println(test5); //false
+        //System.out.println(test3); //NullPointerException
+        //System.out.println(test4); //NullPointerException
+        //System.out.println(test5); //NullPointerException
+        System.out.println(test6); //true
+        System.out.println(test7); //true
+        System.out.println(test8); //true
+        System.out.println(test9); //false
     }
 }
--- src/main/java/GenericsExerciseOneB/Task.java
+++ src/main/java/GenericsExerciseOneB/Task.java
@@ -3,10 +3,7 @@ package main.java.GenericsExerciseOneB;
 public class Task {
     //универсальный метод isEqualTo. Сравнивает два аргумента методом equals()
     public <T, N> boolean isEqualTo(T object1, N object2){
-        if(object1.equals(object2)){
-            return true;
-        } else {
-            return false;
-        }
+        boolean x = object1.equals(object2);
+        return x;
     }
 }
