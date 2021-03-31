a773f6d57b001474565ab81440d6f029ac8c3e04
Pingerock <65583521+Pingerock@users.noreply.github.com>
2021-03-31 12:57:07 +0300

    GenericsExerciseOneB
    Исправленная версия. Попытка №1.
    
    Patch note:
    -исправлено исключение при наличии одного или двух аргументов метода isEqualTo, равных null

--- src/main/java/GenericsExerciseOneB/Main.java
+++ src/main/java/GenericsExerciseOneB/Main.java
@@ -12,21 +12,21 @@ public class Main {
         Object z2 = 550;
         //применяем созданный универсальный метод isEqualTo
         boolean test1, test2, test3, test4, test5, test6, test7, test8, test9;
-        //test1 = task.isEqualTo(x, z);
+        test1 = task.isEqualTo(x, z);
         test2 = task.isEqualTo(x, x1);
-        //test3 = task.isEqualTo(z, x);
-        //test4 = task.isEqualTo(z, z1);
-        //test5 = task.isEqualTo(z, x1);
+        test3 = task.isEqualTo(z, x);
+        test4 = task.isEqualTo(z, z1);
+        test5 = task.isEqualTo(z, x1);
         test6 = task.isEqualTo(x, z2);
         test7 = task.isEqualTo(z2, x);
         test8 = task.isEqualTo(z2, z1);
         test9 = task.isEqualTo(z2, x1);
         //вывод
-        //System.out.println(test1); //NullPointerException
+        System.out.println(test1); //false (NullPointerException)
         System.out.println(test2); //false
-        //System.out.println(test3); //NullPointerException
-        //System.out.println(test4); //NullPointerException
-        //System.out.println(test5); //NullPointerException
+        System.out.println(test3); //false (NullPointerException)
+        System.out.println(test4); //false (NullPointerException)
+        System.out.println(test5); //false (NullPointerException)
         System.out.println(test6); //true
         System.out.println(test7); //true
         System.out.println(test8); //true
--- src/main/java/GenericsExerciseOneB/Task.java
+++ src/main/java/GenericsExerciseOneB/Task.java
@@ -2,8 +2,15 @@ package main.java.GenericsExerciseOneB;
 
 public class Task {
     //универсальный метод isEqualTo. Сравнивает два аргумента методом equals()
-    public <T, N> boolean isEqualTo(T object1, N object2){
-        boolean x = object1.equals(object2);
-        return x;
+    public <T, N> boolean isEqualTo(T object1, N object2) {
+        if (object1 == null) {
+            if (object2 == null) {
+                return true;
+            }
+            return false;
+        } else {
+            boolean x = object1.equals(object2);
+            return x;
+        }
     }
 }
